1debe69b9355d3b839e96a4e5eaa31b2a2e27e44
YansonIT <yanson06@gmail.com>
2023-02-06 20:29:31 +0200

    dz4 modify and done. I hope ))

--- DZ4.py
+++ DZ4.py
@@ -1,1 +1,15 @@
-DZ 4
+# Обернуты число
+
+num = int(input("Enter number:  "))
+
+first  = num % 10
+second = int(num /10 % 10)
+third = int(num / 100 % 10)
+
+# Перевірка
+#print(first)
+#print(second)
+#print(third)
+
+result = third + second * 10 + first * 100
+print(result)
