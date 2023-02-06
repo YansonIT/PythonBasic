c46c3f993569a302ead39bb9b99164dc16e7341b
YansonIT <yanson06@gmail.com>
2023-02-06 22:59:12 +0200

    dz4 modify and done. I hope ))

--- DZ4.py
+++ DZ4.py
@@ -6,10 +6,10 @@ first  = num % 10
 second = int(num /10 % 10)
 third = int(num / 100 % 10)
 
-# Перевірка
-#print(first)
-#print(second)
-#print(third)
+#   Перевірка
+#  print(first)
+#  print(second)
+#  print(third)
 
 result = third + second * 10 + first * 100
 print(result)
